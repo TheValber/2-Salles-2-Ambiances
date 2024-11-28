e044eb59e84872b6cc8e78e5a6a8a1bd9298a438
Valber <valber.contact@gmail.com>
2024-11-28 14:23:10 +0100

    :construction: Create a minimal main that opens a window

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,58 @@
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+
+// using namespace glimac; // To uncomment later when glimac is included
+
+int window_width = 1200;
+int window_height = 800;
+
 int main(int argc, char **argv)
 {
+    /* Inititalize glfw library */
+    if (!glfwInit())
+    {
+        return -1;
+    }
+
+    /* Create a window and its OpenGL context */
+#ifdef __APPLE__
+    /* We need to explicitly ask for a 3.3 context on Mac */
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#endif
+    GLFWwindow *window = glfwCreateWindow(window_width, window_height, "TP5", nullptr, nullptr);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Intialize glad (loads the OpenGL functions) */
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        return -1;
+    }
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        glClearColor(1.f, 0.5f, 0.5f, 1.f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+
     return 0;
 }
