a0af6ff8e68f3697020c200794d47e407cf84d73
Valber <valber.contact@gmail.com>
2024-11-28 15:27:43 +0100

    :sparkles: Add keys to close the window and to swap polygons display mode (fill or line)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
+#include <iostream>
 
 #include "../include/utils.hpp"
 
@@ -8,6 +9,29 @@
 
 int window_width = 1200;
 int window_height = 800;
+
+static void key_callback(GLFWwindow *window, int key, int /*scancode*/, int action, int /*mods*/)
+{
+    if (action == GLFW_PRESS && key == GLFW_KEY_ESCAPE)
+    {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+    else if (action == GLFW_PRESS && key == GLFW_KEY_F)
+    {
+        static bool isWireframe = false;
+        if (isWireframe)
+        {
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+            std::cout << "FILL" << std::endl;
+        }
+        else
+        {
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+            std::cout << "WIREFRAME" << std::endl;
+        }
+        isWireframe = !isWireframe;
+    }
+}
 
 int main(int argc, char **argv)
 {
@@ -17,6 +41,8 @@ int main(int argc, char **argv)
     {
         return -1;
     }
+
+    glfwSetKeyCallback(window, &key_callback);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
